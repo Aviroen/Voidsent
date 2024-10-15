a0f7d1fa39058ba1304b604ce04f2a028c05d7a5
Aviroen <aviroen@gmail.com>
2024-10-15 11:41:03 -0700

    Pipe dream of boat being a minigame of fighting off monsters

---
+++ TileActions.cs
@@ -0,0 +1,24 @@
+﻿using StardewValley;
+using Microsoft.Xna.Framework;
+
+namespace Voidsent;
+internal class TileActions
+{
+
+    public static bool BoatPassage(GameLocation location, string[] args, Farmer farmer, Point point)
+    {
+        var locationData = Game1.getLocationFromName("Aviroen.Voidsent_ArtificialBeach").GetData();
+        if (locationData is not null &&
+            locationData.CustomFields is not null &&
+            locationData.CustomFields.TryGetValue("Aviroen.Voidsent_Boat", out string? customString)) 
+        {
+            if (bool.TryParse(customString, result: out bool stringActivated) && stringActivated)
+            {
+                return true;
+            }
+        }
+        // stop trying other interactions
+        return true;
+    }
+
+}
--- Voidsent.cs
+++ Voidsent.cs
@@ -2,26 +2,21 @@
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using Voidsent.Monsters;
+using StardewValley.BellsAndWhistles;
+using StardewUI.Framework;
 
 namespace Voidsent
 {
     public class Voidsent : Mod
     {
-        private void SetMyLocationFlags()
-        {
-            if (Game1.getLocationFromName("Aviroen.Voidsent_ArtificialBeach") is GameLocation loc)
-            {
-                // set the IsOutdoor thing in the map props.
-                loc.ignoreDebrisWeather.Value = true;
-                loc.ignoreOutdoorLighting.Value = true;
-            }
-            else
-            {
-                this.Monitor.Log($"Huh, can't find my loction, that's weird.");
-            }
-        }
+        internal static IViewEngine viewEngine = null!;
+        internal static string viewAssetPrefix = null!;
+        internal static IFtmApi ftmApi = null!;
+
         public override void Entry(IModHelper helper)
         {
+            viewAssetPrefix = $"Mods/{ModManifest.UniqueID}/Views";
+
             helper.Events.GameLoop.GameLaunched += OnGameLaunched;
             helper.Events.Input.ButtonPressed += OnButtonPressed;
             helper.Events.GameLoop.SaveLoaded += (_, _) => SetMyLocationFlags(); //netbools
@@ -29,7 +24,11 @@ namespace Voidsent
         }
         public void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
         {
-            var api = Helper.ModRegistry.GetApi<IFtmApi>("Esca.FarmTypeManager");
+            ftmApi = Helper.ModRegistry.GetApi<IFtmApi>("Esca.FarmTypeManager")!;
+            viewEngine = Helper.ModRegistry.GetApi<IViewEngine>("focustense.StardewUI")!;
+            viewEngine.RegisterSprites($"Mods/{ModManifest.UniqueID}/Sprites", "assets/sprites");
+            viewEngine.RegisterViews(viewAssetPrefix, "assets/views");
+            GameLocation.RegisterTileAction($"{ModManifest.UniqueID}_Boat",TileActions.BoatPassage);
         }
         private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
         {
@@ -41,41 +40,22 @@ namespace Voidsent
                 //
             }
         }
-        /*
-        private void OnAssetRequested(object? sender, AssetRequestedEventArgs e)
-        {
-            if (e.Name.IsEquivalentTo("Maps/Aviroen.Voidsent_ArtificialBeach"))
-            {
-                e.Edit(asset =>
-                {
-                    IAssetDataForMap editor = asset.AsMap();
-                    Map map = editor.Data;
-
-                    // your code here
-                    ArtificialBeach artificialBeach = (ArtificialBeach)Game1.getLocationFromName("Aviroen.Voidsent_ArtificialBeach");
-                });
-            }
-        } 
-        from atra
         private void SetMyLocationFlags()
-{
-    if (Game1.getLocationFromName("Aviroen.Voidsent_ArtificialBeach") is GameLocation loc)
-    {  
-        // set the IsOutdoor thing in the map props.
-        loc.ignoreDebrisWeather.Value = true;
-        loc.ignoreOutdoorLighting.Value = true; 
-    }
-    else
-    {
-        this.Monitor.Log($"Huh, can't find my loction, that's weird.");
-    }
-}
-
-public override void Entry(IModHelper helper)
-{
-    helper.GameLoop.SaveLoaded += (_, _) => SetMyLocationFlags();
-    helper.GameLooop.SaveCreated += (_, _) => SetMyLocationFlags();
-}
+        {
+            if (Game1.getLocationFromName($"{ModManifest.UniqueID}_ArtificialBeach") is GameLocation loc)
+            {
+                // set the IsOutdoor thing in the map props.
+                loc.ignoreDebrisWeather.Value = true;
+                loc.ignoreOutdoorLighting.Value = true;
+                //loc.critters.Add(new Crow((int)v.X, (int)v.Y));
+            }
+            else
+            {
+                this.Monitor.Log($"Huh, can't find my loction, that's weird.");
+            }
+        }
+        /*
+        
         */
     }
 }
