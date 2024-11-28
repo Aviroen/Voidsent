e2c3a5cc3ea09e34b464616eb55914e181caf477
Aviroen <aviroen@gmail.com>
2024-11-28 01:14:17 -0800

    i18n fixes, screwing around with a harmony skip prefix crime but failing miserably

--- Monsters/BBEG.cs
+++ Monsters/BBEG.cs
@@ -2,99 +2,10 @@
 using Microsoft.Xna.Framework;
 using StardewValley.Monsters;
 using Microsoft.Xna.Framework.Graphics;
-using Netcode;
-using System.Xml.Serialization;
 
 namespace Voidsent.Monsters;
 public class BBEG : Monster
 {
     //CREATE SMOKE EFFECT FROM THE CAULDRON TO FOLLOW THE POSITION OF THE BOSS
-    [XmlElement("c")]
-    public readonly NetColor c = [];
-    public BBEG()
-        : base()
-    { }
-    public BBEG(string name, Vector2 position)
-        : base(name, position)
-    {
-        Sprite.SpriteHeight = 32;
-        Sprite.UpdateSourceRect();
-        IsWalkingTowardPlayer = true;
-    }
-    public override int takeDamage(int damage, int xTrajectory, int yTrajectory, bool isBomb, double addedPrecision, Farmer who)
-    {
-        return base.takeDamage(damage, xTrajectory, yTrajectory, isBomb, addedPrecision, "clank");
-    }
-    protected override void localDeathAnimation()
-    {
-        base.currentLocation.temporarySprites.Add(new TemporaryAnimatedSprite(46, base.Position, Color.DarkGray, 10, flipped: false, 70f));
-        base.currentLocation.temporarySprites.Add(new TemporaryAnimatedSprite(46, base.Position + new Vector2(-32f, 0f), Color.DarkGray, 10, flipped: false, 70f)
-        {
-            delayBeforeAnimationStart = 300
-        });
-        base.currentLocation.temporarySprites.Add(new TemporaryAnimatedSprite(46, base.Position + new Vector2(32f, 0f), Color.DarkGray, 10, flipped: false, 70f)
-        {
-            delayBeforeAnimationStart = 600
-        });
-        base.currentLocation.localSound("monsterdead");
-        Utility.makeTemporarySpriteJuicier(new TemporaryAnimatedSprite(44, base.Position, Color.MediumPurple, 10)
-        {
-            holdLastFrame = true,
-            alphaFade = 0.01f,
-            interval = 70f
-        }, base.currentLocation);
-        base.localDeathAnimation();
-    }
-
-    public override void draw(SpriteBatch b)
-    {
-        if (!base.IsInvisible && Utility.isOnScreen(base.Position, 128))
-        {
-            int standingY = base.StandingPixel.Y;
-            b.Draw(Game1.shadowTexture, base.getLocalPosition(Game1.viewport) + new Vector2(32f, 42f + base.yOffset), Game1.shadowTexture.Bounds, Color.White, 0f, new Vector2(Game1.shadowTexture.Bounds.Center.X, Game1.shadowTexture.Bounds.Center.Y), 3.5f + scale.Value + base.yOffset / 30f, SpriteEffects.None, (float)(standingY - 1) / 10000f);
-            b.Draw(this.Sprite.Texture, base.getLocalPosition(Game1.viewport) + new Vector2(32f, 48 + base.yJumpOffset), this.Sprite.SourceRect, this.c.Value, base.rotation, new Vector2(8f, 16f), Math.Max(0.2f, scale.Value) * 4f, base.flip ? SpriteEffects.FlipHorizontally : SpriteEffects.None, Math.Max(0f, base.drawOnTop ? 0.991f : ((float)standingY / 10000f)));
-        }
-    }
-
-    public override void shedChunks(int number, float scale)
-    {
-        Point standingPixel = base.StandingPixel;
-        Game1.createRadialDebris(base.currentLocation, this.Sprite.textureName.Value, new Rectangle(0, this.Sprite.getHeight() * 4, 16, 16), 8, standingPixel.X, standingPixel.Y, number, base.TilePoint.Y, Color.White, scale * 4f);
-    }
-
-    public override List<Item> getExtraDropItems()
-    {
-        List<Item> extraItems = [];
-        if ((Game1.stats.getMonstersKilled(name.Value) + (int)Game1.uniqueIDForThisGame) % 100 == 0)
-        {
-            extraItems.Add(ItemRegistry.Create("(H)51"));
-        }
-        return extraItems;
-    }
-
-    protected override void updateMonsterSlaveAnimation(GameTime time)
-    {
-        if (this.isMoving())
-        {
-            switch (this.FacingDirection)
-            {
-                case 0:
-                    this.Sprite.AnimateUp(time);
-                    break;
-                case 3:
-                    this.Sprite.AnimateLeft(time);
-                    break;
-                case 1:
-                    this.Sprite.AnimateRight(time);
-                    break;
-                case 2:
-                    this.Sprite.AnimateDown(time);
-                    break;
-            }
-        }
-        else
-        {
-            this.Sprite.StopAnimation();
-        }
-    }
+    
 }
--- Voidsent.cs
+++ Voidsent.cs
@@ -2,6 +2,7 @@
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewUI.Framework;
+using HarmonyLib;
 
 namespace Voidsent;
 public class Voidsent : Mod
@@ -10,6 +11,22 @@ public class Voidsent : Mod
     //internal static string viewAssetPrefix = null!;
     internal static IFtmApi ftmApi = null!;
     private IViewEngine viewEngine = null!;
+    internal static IModHelper ModHelper { get; set; } = null!;
+    internal static IMonitor ModMonitor { get; set; } = null!;
+    internal static Harmony Harmony { get; set; } = null!;
+    internal static IManifest Manifest { get; set; } = null!;
+
+    static List<string> voidsentLocations = new()
+    {
+        "Aviroen.VoidsentCP_ArtificialBeach",
+        "Aviroen.VoidsentCP_Commonwealth",
+        "Aviroen.VoidsentCP_CrimsonGrove",
+        "Aviroen.VoidsentCP_Grandberg",
+        "Aviroen.VoidsentCP_Grove",
+        "Aviroen.VoidsentCP_Morabyr",
+        "Aviroen.VoidsentCP_Outlands",
+        "Aviroen.VoidsentCP_Boat",
+    };
 
     public override void Entry(IModHelper helper)
     {
@@ -19,6 +36,13 @@ public class Voidsent : Mod
         helper.Events.Input.ButtonPressed += OnButtonPressed;
         helper.Events.GameLoop.SaveLoaded += (_, _) => SetMyLocationFlags(); //netbools
         helper.Events.GameLoop.SaveCreated += (_, _) => SetMyLocationFlags(); //netbools
+
+        ModHelper = helper;
+        ModMonitor = Monitor;
+        Harmony = new Harmony(ModManifest.UniqueID);
+        Manifest = ModManifest;
+
+        Harmony.PatchAll();
     }
     public void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
     {
@@ -40,19 +64,39 @@ public class Voidsent : Mod
     }
     private void SetMyLocationFlags()
     {
-        if (Game1.getLocationFromName($"{ModManifest.UniqueID}_CrimsonGrove") is GameLocation loc)
+        for (int i = 0; i < voidsentLocations.Count; i++)
         {
-            // set the IsOutdoor thing in the map props.
-            loc.ignoreDebrisWeather.Value = false;
-            loc.ignoreOutdoorLighting.Value = true;
-            //loc.critters.Add(new Crow((int)v.X, (int)v.Y));
-            //FIND THE EYEBLINKING FROM FARMCAVE TO ADD TO THE CRIMSONGROVE/GROVE
-        }
-        else
-        {
-            this.Monitor.Log($"Huh, can't find my loction, that's weird.");
+            var location = voidsentLocations[i];
+            if (Game1.getLocationFromName(location) is GameLocation loc)
+            {
+                // set the IsOutdoor thing in the map props.
+                loc.ignoreDebrisWeather.Value = false;
+                loc.ignoreOutdoorLighting.Value = true;
+                //loc.critters.Add(new Crow((int)v.X, (int)v.Y));
+                //FIND THE EYEBLINKING FROM FARMCAVE TO ADD TO THE CRIMSONGROVE/GROVE
+                //fireflies
+                //other ambient wiggly lights they're probably wisps
+            }
+            else
+            {
+                this.Monitor.Log($"Huh, can't find my loction, that's weird.");
+            }
         }
     }
+
+    [HarmonyPatch(typeof(GameLocation), "ShowLockedDoorMessage")]
+    public static bool Prefix()
+    {
+        foreach (var location in voidsentLocations)
+        {
+            if (Game1.currentLocation is location)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
     /*
     you could like. just force teleport them out after a certain amount of time
     not sure how you mean? countdown in updateWhenCurrentLocation or OnUpdateTicked, boot them out after a while
