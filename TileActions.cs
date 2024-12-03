28a22ae063fe929f601c441471c2ca8647794676
Aviroen <aviroen@gmail.com>
2024-12-03 03:08:42 -0800

    nonbinary doors

--- TileActions.cs
+++ TileActions.cs
@@ -5,22 +5,28 @@ using static StardewValley.DelayedAction;
 namespace Voidsent;
 internal class TileActions
 {
-
     public static bool BoatPassage(GameLocation location, string[] args, Farmer farmer, Point point)
     { //beach -> boat -> boat transition map of timer -> port morabyr
-        var locationData = Game1.getLocationFromName("Aviroen.Voidsent_ArtificialBeach").GetData();
+        var locationData = Game1.getLocationFromName("Aviroen.VoidsentCP_ArtificialBeach").GetData();
         if (locationData is not null &&
             locationData.CustomFields is not null &&
-            locationData.CustomFields.TryGetValue("Aviroen.Voidsent_Boat", out string? customString))
+            locationData.CustomFields.TryGetValue("Aviroen.VoidsentCP_Boat", out string? customString))
         {
             if (bool.TryParse(customString, result: out bool stringActivated) && stringActivated)
             {
-                warpAfterDelay("Aviroen.Voidsent_Morabyr", point, 60000);
+                warpAfterDelay("Aviroen.VoidsentCP_Morabyr", point, 60000);
                 return true;
             }
         }
         // stop trying other interactions
         return true;
     }
+    /*
+you could like. just force teleport them out after a certain amount of time
+not sure how you mean? countdown in updateWhenCurrentLocation or OnUpdateTicked, boot them out after a while
+i don't remember if delayedaction respects pauses and time passing and such, which you could check for per-tick
+delayedactions get checked in UpdateOther it seems which only gets called if the host is not paused. (or if gameMode == 10?)
 
+implement 'safe route' which takes longer
+*/
 }
--- Voidsent.cs
+++ Voidsent.cs
@@ -3,27 +3,29 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewUI.Framework;
 using HarmonyLib;
-using StardewValley.TokenizableStrings;
+using Voidsent.Patches;
+using StardewValley.Locations;
 
-namespace Voidsent;
-[HarmonyPatch]
-public class Voidsent : Mod
+namespace Voidsent
 {
-    //internal static IViewEngine viewEngine = null!;
-    //internal static string viewAssetPrefix = null!;
-    /* iterate a list of all npcs with {{ModId}}_ to check per 8 heart and flag //might not need this
-     * IncrementStat custom stat Aviroen.VoidsentCP_Fighters
-     * use c# to check how many that is
-     * divide the boss health 2% per int
-     */
-    internal static IFtmApi ftmApi = null!;
-    private IViewEngine viewEngine = null!;
-    internal static IModHelper ModHelper { get; set; } = null!;
-    internal static IMonitor ModMonitor { get; set; } = null!;
-    internal static Harmony Harmony { get; set; } = null!;
-    internal static IManifest Manifest { get; set; } = null!;
-    List<string> locNames = new() 
-    { 
+    public class Voidsent : Mod
+    {
+        //internal static IViewEngine viewEngine = null!;
+        //internal static string viewAssetPrefix = null!;
+        /* iterate a list of all npcs with {{ModId}}_ to check per 8 heart and flag //might not need this
+         * IncrementStat custom stat Aviroen.VoidsentCP_Fighters
+         * use c# to check how many that is
+         * divide the boss health 2% per int
+         */
+        internal static IFtmApi ftmApi = null!;
+        private IViewEngine viewEngine = null!;
+        internal static IModHelper ModHelper { get; set; } = null!;
+        internal static IMonitor ModMonitor { get; set; } = null!;
+        internal static Harmony Harmony { get; set; } = null!;
+        internal static IManifest Manifest { get; set; } = null!;
+        private List<Wisp>? _wisps;
+        List<string> locNames = new()
+    {
         "Aviroen.VoidsentCP_ArtificialBeach",
         "Aviroen.VoidsentCP_Commonwealth",
         "Aviroen.VoidsentCP_CrimsonGrove",
@@ -36,111 +38,75 @@ public class Voidsent : Mod
         "Aviroen.VoidsentCP_EdelweissAttic",
         "Aviroen.VoidsentCP_EdelweissBasement",
     };
-    static List<GameLocation> myLocations = new();
-    public override void Entry(IModHelper helper)
-    {
-        //viewAssetPrefix = $"Mods/{ModManifest.UniqueID}/Views";
-
-        helper.Events.GameLoop.GameLaunched += OnGameLaunched;
-        helper.Events.Input.ButtonPressed += OnButtonPressed;
-        helper.Events.GameLoop.SaveLoaded += (_, _) => SetMyLocationFlags(); //netbools
-        helper.Events.GameLoop.SaveCreated += (_, _) => SetMyLocationFlags(); //netbools
-        helper.Events.GameLoop.DayStarted += OnDayStarted;
-
-        ModHelper = helper;
-        ModMonitor = Monitor;
-        Harmony = new Harmony(ModManifest.UniqueID);
-        Manifest = ModManifest;
-
-        Harmony.PatchAll();
-    }
-    public void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
-    {
-        ftmApi = Helper.ModRegistry.GetApi<IFtmApi>("Esca.FarmTypeManager")!;
-        //viewEngine = Helper.ModRegistry.GetApi<IViewEngine>("focustense.StardewUI")!;
-        //viewEngine.RegisterSprites($"Mods/{ModManifest.UniqueID}/Sprites", "Assets/Sprites");
-        //viewEngine.RegisterViews($"Mods/{ModManifest.UniqueID}/Views", "Assets/Views");
-        GameLocation.RegisterTileAction($"{ModManifest.UniqueID}_Boat", TileActions.BoatPassage);
-    }
-    private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
-    {
-        if (!Context.IsWorldReady)
-            return;
-
-        if (e.Button == SButton.F5)
+        public static List<GameLocation> myLocations = new();
+        public override void Entry(IModHelper helper)
         {
-            //
+            //viewAssetPrefix = $"Mods/{ModManifest.UniqueID}/Views";
+
+            helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+            helper.Events.Input.ButtonPressed += OnButtonPressed;
+            helper.Events.GameLoop.SaveLoaded += (_, _) => SetMyLocationFlags(); //netbools
+            helper.Events.GameLoop.SaveCreated += (_, _) => SetMyLocationFlags(); //netbools
+            helper.Events.GameLoop.DayStarted += OnDayStarted;
+
+            ModHelper = helper;
+            ModMonitor = Monitor;
+            Harmony = new Harmony(ModManifest.UniqueID);
+            Manifest = ModManifest;
+
+            Harmony.Patch(
+                original: AccessTools.Method(typeof(GameLocation), nameof(GameLocation.ShowLockedDoorMessage)),
+                prefix: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.Prefix)));
+            //Harmony.PatchAll();
         }
-    }
-    private void SetMyLocationFlags()
-    {
-        for (int i = 0; i < locNames.Count; i++)
+        public void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
         {
-            var location = locNames[i];
-            if (Game1.getLocationFromName(location) is GameLocation loc)
+            ftmApi = Helper.ModRegistry.GetApi<IFtmApi>("Esca.FarmTypeManager")!;
+            //viewEngine = Helper.ModRegistry.GetApi<IViewEngine>("focustense.StardewUI")!;
+            //viewEngine.RegisterSprites($"Mods/{ModManifest.UniqueID}/Sprites", "Assets/Sprites");
+            //viewEngine.RegisterViews($"Mods/{ModManifest.UniqueID}/Views", "Assets/Views");
+            GameLocation.RegisterTileAction($"{ModManifest.UniqueID}_Boat", TileActions.BoatPassage);
+
+        }
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (e.Button == SButton.F5)
             {
-                // set the IsOutdoor thing in the map props.
-                loc.ignoreDebrisWeather.Value = false;
-                loc.ignoreOutdoorLighting.Value = true;
-                //loc.critters.Add(new Crow((int)v.X, (int)v.Y));
-                //FIND THE EYEBLINKING FROM FARMCAVE TO ADD TO THE CRIMSONGROVE/GROVE
-                //fireflies
-                //other ambient wiggly lights they're probably wisps
-            }
-            else
-            {
-                this.Monitor.Log($"Huh, can't find my loction, that's weird.");
+                //
             }
         }
-    }
-
-    private void OnDayStarted(object? sender, DayStartedEventArgs e)
-    {
-        myLocations.Clear();
-        foreach (var l in locNames)
+        private void SetMyLocationFlags()
         {
-            myLocations.Add(Game1.getLocationFromName(l));
-        }
-    }
-    //many thanks to rokugin for helping set this one up
-    [HarmonyPatch(typeof(GameLocation), "ShowLockedDoorMessage")]
-    public static bool Prefix(GameLocation __instance, string[] action)
-    {
-        if (myLocations.Contains(__instance))
-        {
-            Gender ownerGender = Gender.Female;
-            string[] ownerNames = new string[(action.Length == 2) ? 1 : 2];
-            for (int i = 0; i < ownerNames.Length; i++)
+            for (int i = 0; i < locNames.Count; i++)
             {
-                string ownerKey = action[i + 1];
-                NPC owner = Game1.getCharacterFromName(ownerKey);
-                if (owner != null)
+                var location = locNames[i];
+                if (Game1.getLocationFromName(location) is GameLocation loc)
                 {
-                    ownerNames[i] = owner.displayName;
-                    ownerGender = owner.Gender;
-                    continue;
+                    //loc.treatAsOutdoors.Value = true;
+                    loc.ignoreDebrisWeather.Value = false;
+                    loc.ignoreOutdoorLighting.Value = true;
+                    //loc.critters.Add(new Crow((int)v.X, (int)v.Y));
+                    //FIND THE EYEBLINKING FROM FARMCAVE TO ADD TO THE CRIMSONGROVE/GROVE
+                    //fireflies
+                    //other ambient wiggly lights they're probably wisps
                 }
-                if (NPC.TryGetData(ownerKey, out var data))
+                else
                 {
-                    ownerNames[i] = TokenParser.ParseText(data.DisplayName);
-                    ownerGender = data.Gender;
-                    continue;
+                    this.Monitor.Log($"Huh, can't find my loction, that's weird.");
                 }
             }
-            string lockedDoorMessage = ((ownerNames.Length <= 1) ? Game1.content.LoadString("Strings\\Locations:DoorUnlock_NotFriend_" + ((ownerGender == Gender.Male) ? "Male" : (ownerGender == Gender.Female) ? "Female" : "Undefined"), ownerNames[0]) : Game1.content.LoadString("Strings\\Locations:DoorUnlock_NotFriend_Couple", ownerNames[0], ownerNames[1]));
-
-            Game1.drawObjectDialogue(lockedDoorMessage);
-            return false;
         }
-        return true;
+
+        private void OnDayStarted(object? sender, DayStartedEventArgs e)
+        {
+            myLocations.Clear();
+            foreach (var l in locNames)
+            {
+                myLocations.Add(Game1.getLocationFromName(l));
+            }
+        }
     }
-
-    /*
-    you could like. just force teleport them out after a certain amount of time
-    not sure how you mean? countdown in updateWhenCurrentLocation or OnUpdateTicked, boot them out after a while
-    i don't remember if delayedaction respects pauses and time passing and such, which you could check for per-tick
-    delayedactions get checked in UpdateOther it seems which only gets called if the host is not paused. (or if gameMode == 10?)
-
-    implement 'safe route' which takes longer
-    */
 }
