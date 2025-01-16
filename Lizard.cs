b45ce6a7454319e0961b97a9c932212da004d145
irocendar <97824271+irocendar@users.noreply.github.com>
2025-01-16 05:52:23 +0000

    Update Lizard.cs

--- Lizard.cs
+++ Lizard.cs
@@ -1,56 +1,82 @@
-﻿using StardewValley.Characters;
+using HarmonyLib;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
 using StardewValley;
-using StardewValley.Buildings;
+using StardewValley.Characters;
 
-namespace Voidsent
+namespace Voidsent;
+
+internal static class GuidHelper
 {
-    internal class Lizard : Horse
+    public static Guid NewGuid()
     {
-        public Lizard()
+        return Guid.NewGuid();
+    }
+}
+
+internal sealed class ModEntry : Mod
+{
+    /// <inheritdoc/>
+    public override void Entry(IModHelper helper)
+    {
+        helper.Events.Content.AssetRequested += this.OnAssetRequested;
+        helper.ConsoleCommands.Add("lizard", "Spawn lizard.", this.Lizard);
+        GameLocation.RegisterTileAction("Aviroen.Voidsent_SummonLizard", this.HandleSummonLizard);
+        
+        var harmony = new Harmony(this.ModManifest.UniqueID);
+    }
+
+    private bool HandleSummonLizard(GameLocation location, string[] args, Farmer player, Point point)
+    {
+        string displayPrice = Utility.getNumberWithCommas(1000);
+        location.createQuestionDialogue(
+            $"Rent a lizard for {displayPrice}g?", 
+            location.createYesNoResponses(), 
+            (Farmer who, string answer) => LizardAnswer(who, answer, location, point),
+            null
+            );
+        return true;
+    }
+
+    private void LizardAnswer(Farmer who, string answer, GameLocation loc, Point point)
+    {
+        if (answer == "Yes")
         {
-            ateCarrotToday = false;
-            base.willDestroyObjectsUnderfoot = false;
-            base.HideShadow = true;
-            base.drawOffset = new Vector2(-16f, 0f);
-            this.onFootstepAction = PerformDefaultHorseFootstep;
-            this.faceDirection(3);
-            base.Breather = false;
-            base.initNetFields();
-        }
-        public override void ChooseAppearance(LocalizedContentManager? content = null)
-        {
-            if (this.Sprite == null)
+            if (Game1.player.Money < 1000)
             {
-                this.Sprite = new AnimatedSprite("Animals\\Aviroen.VoidsentCP_Komodo", 0, 32, 32);
-                this.Sprite.textureUsesFlippedRightForLeft = true;
-                this.Sprite.loop = true;
+                Game1.drawObjectDialogue(Game1.content.LoadString("Strings\\Locations:BusStop_NotEnoughMoneyForTicket"));
+            }
+            else
+            {
+                Game1.player.Money -= 1000;
+                var horse = new Horse(GuidHelper.NewGuid(), point.X + 1, point.Y);
+                loc.characters.Add(horse);
+                horse.Sprite.LoadTexture("Animals\\Aviroen.VoidsentCP_Komodo", syncTextureName: false);
+                if (Game1.player.IsBusyDoingSomething())
+                {
+                    Game1.actionsWhenPlayerFree.Add(() => horse.checkAction(who, loc));
+                }
+                else
+                {
+                    horse.checkAction(who, loc);
+                }
             }
         }
+    }
 
-        public new bool TryFindStable(out GameLocation location, out Stable stable)
+    private void Lizard(string command, string[] args)
+    {
+        if (!ArgUtility.TryGetOptionalInt(args, 0, out var tileX, out var error, Game1.player.TilePoint.X, "int tileX") || !ArgUtility.TryGetOptionalInt(args, 0, out var tileY, out error, Game1.player.TilePoint.Y, "int tileY"))
         {
-            GameLocation? foundLocation = null;
-            Stable? foundStable = null;
-            Utility.ForEachLocation(delegate (GameLocation curLocation)
-            {
-                foreach (Building building in curLocation.buildings)
-                {
-                    if (building is Stable stable2 && stable2.HorseId == this.HorseId && !stable2.isUnderConstruction())
-                    {
-                        foundLocation = curLocation;
-                        foundStable = stable2;
-                        if (curLocation.IsActiveLocation())
-                        {
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            });
-            location = foundLocation!;
-            stable = foundStable!;
-            return stable != null;
+            Monitor.Log($"Invalid argument: {command}", LogLevel.Warn);
+        }
+        else
+        {
+            var horse = new Horse(GuidHelper.NewGuid(), tileX, tileY);
+            Game1.currentLocation.characters.Add(horse);
+            horse.Sprite.LoadTexture("Animals\\Aviroen.VoidsentCP_Komodo", syncTextureName: false);
         }
     }
 }
