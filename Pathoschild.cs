c50c4aa42aa686b3d2268bbe89528402b77a545c
Aviroen <aviroen@gmail.com>
2025-02-08 08:02:48 -0800

    OUGH: All interiors to have the new endless yapping C# component courtesy of reading https://github.com/Pathoschild/StardewMods/tree/develop/CentralStation Duvin, Fothao, and Thandael sprites done Added all of the keys to the custom asset for endless yapping Building Layer TileAction

--- Pathoschild.cs
+++ Pathoschild.cs
@@ -34,6 +34,13 @@ SOFTWARE.
         private static string ModID { get; set; }
         public const string Bookshelf = "Bookshelf";
         public const string Kitchen = "Kitchen";
+        public const string Dresser = "Dresser";
+        public const string Dining = "Dining";
+        public const string Cabinet = "Cabinet";
+        public const string TV = "TV";
+        public const string Clock = "Clock";
+        public const string Coat = "Coat";
+        public const string Misc = "Misc";
         public static void Initialize(IModRegistry registry, string modID)
         {
             ModRegistry = registry;
@@ -41,8 +48,8 @@ SOFTWARE.
         }
         public static bool OnCentralAction(GameLocation location, string[] args, Farmer who, Point tile)
         {
-            if (location.NameOrUniqueName is not $"Aviroen.VoidsentCP_")
-                return false;
+           // if (location.NameOrUniqueName is not $"Aviroen.VoidsentCP_")
+                //return false;
 
             string subAction = ArgUtility.Get(args, 1);
             switch (subAction)
@@ -51,6 +58,20 @@ SOFTWARE.
                     return BookshelfBool();
                 case Kitchen:
                     return KitchenBool();
+                case Dresser:
+                    return DresserBool();
+                case Dining:
+                    return DiningBool();
+                case Cabinet:
+                    return CabinetBool();
+                case TV:
+                    return TVBool();
+                case Clock:
+                    return ClockBool();
+                case Coat:
+                    return CoatBool();
+                case Misc:
+                    return MiscBool();
 
                 default:
                     return false;
@@ -59,18 +80,6 @@ SOFTWARE.
         public static bool BookshelfBool()
         {
             string message = GetBookshelfMessage();
-
-            if (!string.IsNullOrWhiteSpace(message))
-            {
-                Game1.drawDialogueNoTyping(message);
-                return true;
-            }
-
-            return false;
-        }
-        public static bool KitchenBool()
-        {
-            string message = GetKitchenMessage();
 
             if (!string.IsNullOrWhiteSpace(message))
             {
@@ -104,6 +113,18 @@ SOFTWARE.
             seenMessages.Add(selected);
             return selected;
         }
+        public static bool KitchenBool()
+        {
+            string message = GetKitchenMessage();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Game1.drawDialogueNoTyping(message);
+                return true;
+            }
+
+            return false;
+        }
         public static string GetKitchenMessage()
         {
             HashSet<string> seenMessages = new();
@@ -121,6 +142,258 @@ SOFTWARE.
             {
                 seenMessages.Clear();
                 return GetKitchenMessage();
+            }
+
+            // choose one
+            string selected = Game1.random.ChooseFrom(options);
+            seenMessages.Add(selected);
+            return selected;
+        }
+        public static bool DresserBool()
+        {
+            string message = GetDresserMessage();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Game1.drawDialogueNoTyping(message);
+                return true;
+            }
+
+            return false;
+        }
+        public static string GetDresserMessage()
+        {
+            HashSet<string> seenMessages = new();
+
+            // get available options
+            List<string> options = new();
+            foreach ((string id, List<string>? dialogues) in Game1.content.Load<Dictionary<string, List<string>?>>($"Mods/{ModID}/Dresser"))
+            {
+                options.AddRange(dialogues ?? []);
+            }
+            options.RemoveAll(option => string.IsNullOrWhiteSpace(option) || seenMessages.Contains(option));
+
+            // if we've seen them all, reset
+            if (options.Count == 0 && seenMessages.Count > 0)
+            {
+                seenMessages.Clear();
+                return GetDresserMessage();
+            }
+
+            // choose one
+            string selected = Game1.random.ChooseFrom(options);
+            seenMessages.Add(selected);
+            return selected;
+        }
+        public static bool DiningBool()
+        {
+            string message = GetDiningMessage();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Game1.drawDialogueNoTyping(message);
+                return true;
+            }
+
+            return false;
+        }
+        public static string GetDiningMessage()
+        {
+            HashSet<string> seenMessages = new();
+
+            // get available options
+            List<string> options = new();
+            foreach ((string id, List<string>? dialogues) in Game1.content.Load<Dictionary<string, List<string>?>>($"Mods/{ModID}/Dining"))
+            {
+                options.AddRange(dialogues ?? []);
+            }
+            options.RemoveAll(option => string.IsNullOrWhiteSpace(option) || seenMessages.Contains(option));
+
+            // if we've seen them all, reset
+            if (options.Count == 0 && seenMessages.Count > 0)
+            {
+                seenMessages.Clear();
+                return GetDiningMessage();
+            }
+
+            // choose one
+            string selected = Game1.random.ChooseFrom(options);
+            seenMessages.Add(selected);
+            return selected;
+        }
+        public static bool CabinetBool()
+        {
+            string message = GetCabinetMessage();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Game1.drawDialogueNoTyping(message);
+                return true;
+            }
+
+            return false;
+        }
+        public static string GetCabinetMessage()
+        {
+            HashSet<string> seenMessages = new();
+
+            // get available options
+            List<string> options = new();
+            foreach ((string id, List<string>? dialogues) in Game1.content.Load<Dictionary<string, List<string>?>>($"Mods/{ModID}/Cabinet"))
+            {
+                options.AddRange(dialogues ?? []);
+            }
+            options.RemoveAll(option => string.IsNullOrWhiteSpace(option) || seenMessages.Contains(option));
+
+            // if we've seen them all, reset
+            if (options.Count == 0 && seenMessages.Count > 0)
+            {
+                seenMessages.Clear();
+                return GetCabinetMessage();
+            }
+
+            // choose one
+            string selected = Game1.random.ChooseFrom(options);
+            seenMessages.Add(selected);
+            return selected;
+        }
+        public static bool TVBool()
+        {
+            string message = GetTVMessage();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Game1.drawDialogueNoTyping(message);
+                return true;
+            }
+
+            return false;
+        }
+        public static string GetTVMessage()
+        {
+            HashSet<string> seenMessages = new();
+
+            // get available options
+            List<string> options = new();
+            foreach ((string id, List<string>? dialogues) in Game1.content.Load<Dictionary<string, List<string>?>>($"Mods/{ModID}/TV"))
+            {
+                options.AddRange(dialogues ?? []);
+            }
+            options.RemoveAll(option => string.IsNullOrWhiteSpace(option) || seenMessages.Contains(option));
+
+            // if we've seen them all, reset
+            if (options.Count == 0 && seenMessages.Count > 0)
+            {
+                seenMessages.Clear();
+                return GetTVMessage();
+            }
+
+            // choose one
+            string selected = Game1.random.ChooseFrom(options);
+            seenMessages.Add(selected);
+            return selected;
+        }
+        public static bool ClockBool()
+        {
+            string message = GetClockMessage();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Game1.drawDialogueNoTyping(message);
+                return true;
+            }
+
+            return false;
+        }
+        public static string GetClockMessage()
+        {
+            HashSet<string> seenMessages = new();
+
+            // get available options
+            List<string> options = new();
+            foreach ((string id, List<string>? dialogues) in Game1.content.Load<Dictionary<string, List<string>?>>($"Mods/{ModID}/Clock"))
+            {
+                options.AddRange(dialogues ?? []);
+            }
+            options.RemoveAll(option => string.IsNullOrWhiteSpace(option) || seenMessages.Contains(option));
+
+            // if we've seen them all, reset
+            if (options.Count == 0 && seenMessages.Count > 0)
+            {
+                seenMessages.Clear();
+                return GetClockMessage();
+            }
+
+            // choose one
+            string selected = Game1.random.ChooseFrom(options);
+            seenMessages.Add(selected);
+            return selected;
+        }
+        public static bool CoatBool()
+        {
+            string message = GetCoatMessage();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Game1.drawDialogueNoTyping(message);
+                return true;
+            }
+
+            return false;
+        }
+        public static string GetCoatMessage()
+        {
+            HashSet<string> seenMessages = new();
+
+            // get available options
+            List<string> options = new();
+            foreach ((string id, List<string>? dialogues) in Game1.content.Load<Dictionary<string, List<string>?>>($"Mods/{ModID}/Coat"))
+            {
+                options.AddRange(dialogues ?? []);
+            }
+            options.RemoveAll(option => string.IsNullOrWhiteSpace(option) || seenMessages.Contains(option));
+
+            // if we've seen them all, reset
+            if (options.Count == 0 && seenMessages.Count > 0)
+            {
+                seenMessages.Clear();
+                return GetCoatMessage();
+            }
+
+            // choose one
+            string selected = Game1.random.ChooseFrom(options);
+            seenMessages.Add(selected);
+            return selected;
+        }
+        public static bool MiscBool()
+        {
+            string message = GetMiscMessage();
+
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Game1.drawDialogueNoTyping(message);
+                return true;
+            }
+
+            return false;
+        }
+        public static string GetMiscMessage()
+        {
+            HashSet<string> seenMessages = new();
+
+            // get available options
+            List<string> options = new();
+            foreach ((string id, List<string>? dialogues) in Game1.content.Load<Dictionary<string, List<string>?>>($"Mods/{ModID}/Misc"))
+            {
+                options.AddRange(dialogues ?? []);
+            }
+            options.RemoveAll(option => string.IsNullOrWhiteSpace(option) || seenMessages.Contains(option));
+
+            // if we've seen them all, reset
+            if (options.Count == 0 && seenMessages.Count > 0)
+            {
+                seenMessages.Clear();
+                return GetMiscMessage();
             }
 
             // choose one
